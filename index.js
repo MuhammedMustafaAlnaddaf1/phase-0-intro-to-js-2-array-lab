fd51b43c86e8c717c16f9830d998342edb286d31
Muhammed Mustafa Alnaddaf <alnaddafmustafa3@gamil.com>
2023-04-01 16:18:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+function destructivelyRemoveFirstCat(name) {
+  cats.shift();
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function appendCat(name) {
+  let catsCopy = cats.slice();
+  catsCopy.push(name);
+  return catsCopy;
+}
+
+function prependCat(name) {
+  let catsCopy = cats.slice();
+  catsCopy.unshift(name);
+  return catsCopy;
+}
+
+function removeLastCat(name) {
+  let catsCopy = cats.slice();
+  catsCopy.pop();
+  return catsCopy;
+}
+
+function removeFirstCat(name) {
+  let catsCopy = cats.slice();
+  catsCopy.shift();
+  return catsCopy;
+}
